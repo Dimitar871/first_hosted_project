a500defd0b6dbbdc8958628c7e2ae70e570a13e3
Dimitar871 <180311631+Dimitar871@users.noreply.github.com>
2025-06-14 18:57:51 +0200

    Pass tasks to welcome view

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
-use App\Http\Controllers\TaskController;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::all();
+    return view('welcome', compact('tasks'));
 });
-
 
 Route::resource('tasks', TaskController::class);
